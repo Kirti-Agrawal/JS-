0568b3db5270b6a0321d18ae6dec8917f7201e04
Kirti-Agrawal <121790445+Kirti-Agrawal@users.noreply.github.com>
2025-06-06 11:52:57 +0000

    Nums and Math

---
+++ 01_Basics/06_nums_maths.js
@@ -0,0 +1,13 @@
+//Ecommerce
+let selectedNum = -90;
+console.log(Math.abs(selectedNum));
+
+let total = -90.8989;
+console.log(Math.round(total));
+console.log(Math.max(selectedNum,total));
+
+console.log((Math.random()*10) + 1);  //not good 0.0019 if need value 10 to 20
+let min = 100;
+let max = 200;
+console.log(Math.floor(Math.random()* (max-min+1) + min));
+
