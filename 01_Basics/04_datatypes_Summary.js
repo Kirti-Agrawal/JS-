4d5f3e4bb43cf2a73a12624ae12f764d2895bb4f
Kirti-Agrawal <121790445+Kirti-Agrawal@users.noreply.github.com>
2025-06-06 07:26:52 +0000

    DataTypes summary  for Interview

---
+++ 01_Basics/04_datatypes_Summary.js
@@ -0,0 +1,11 @@
+// Datatypes 
+
+// Primitive - 7 types -  string , int , boolean , null (empty) ,undefined , BigInt , Symbol 
+//Non-Priitive - Array  , Object , Function
+
+let fun1 = function (){
+
+}
+let nullValue = null;
+
+console.log(typeof fun1, typeof nullValue);
