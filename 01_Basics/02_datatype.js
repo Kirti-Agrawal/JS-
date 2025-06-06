5a601bbfbc438d4ce7c72dac6db3bbbbcce11eef
Kirti-Agrawal <121790445+Kirti-Agrawal@users.noreply.github.com>
2025-06-06 07:05:06 +0000

    Basics like Comparision ,datatypes

---
+++ 01_Basics/02_datatype.js
@@ -0,0 +1,31 @@
+console.log(typeof "kirti");
+
+let name = "Kirti";
+
+let valueName = Number(name);
+console.log( typeof valueName);   ///Datatype should not be changed
+
+let value = Boolean("kirti");
+console.log(value);
+
+
+//Conversion
+
+// "irti"/undefined =>  Nan
+// "33" => 33
+// true/false/null => 1/0
+
+//"" => 0
+// "ki" => 1
+// true => 1
+
+
+//*******************************************************Operations************************************* */
+
+let negValue = -value;
+console.log(negValue);
+///Operations = > Cryptography
+
+
+//Not needed (1+ "2" + 2)
+//log(+true)   Not expected
---
+++ 01_Basics/03_comparison.js
@@ -0,0 +1,8 @@
+// when data types different -> Unpredictable Results
+
+console.log("33" == 33 );
+console.log(null >= 0); //Avoid
+
+console.log(null == 0);
+
+// === same data type
